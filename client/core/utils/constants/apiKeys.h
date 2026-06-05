e746638239dcd30b77d4d98e098ca4aad5cfb6df
NickVs2015 <nv@amnezia.org>
2026-06-05 14:10:53 +0300

    fix: add support market types, add logs

--- client/core/utils/constants/apiKeys.h
+++ client/core/utils/constants/apiKeys.h
@@ -40,6 +40,7 @@ namespace apiDefs
         constexpr QLatin1String lastDownloaded("last_downloaded");
         constexpr QLatin1String sourceType("source_type");
         constexpr QLatin1String appLanguage("app_language");
+        constexpr QLatin1String market("market");
 
         constexpr QLatin1String activeDeviceCount("active_device_count");
         constexpr QLatin1String maxDeviceCount("max_device_count");
